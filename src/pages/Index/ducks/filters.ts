5ba3e7a5fa39df120298471df9b4b0c9f4c6f27e
Matheus Eduardo Barboza Santos <38351837+matheuseduardo09@users.noreply.github.com>
2020-06-04 21:55:54 -0300

    Created Type and Creators using method ducks for index

---
+++ src/pages/Index/ducks/filters.ts
@@ -0,0 +1,45 @@
+import { createActions, createReducer } from "reduxsauce";
+
+interface InitialState {
+  type: string;
+  name: string;
+}
+
+export interface FilterTypeAction {
+  type: string
+}
+
+interface FilterNameElement {
+  name: string
+}
+
+export const { Types, Creators } = createActions({
+  filterType: [ 'type' ],
+  filterTypeSaga: [ 'type' ],
+  filterNameElement: [ 'name' ],
+  filterNameElementSaga: [ 'name' ],
+});
+
+const INITIAL_STATE = {
+  type: '',
+  name: ''
+}
+
+const setFilterType = ( state: InitialState = INITIAL_STATE, action: FilterTypeAction ) => {
+  return {
+    ...state,
+    type: action.type
+  }
+}
+
+const setFilterNameElement = ( state: InitialState = INITIAL_STATE, action: FilterNameElement ) => {
+  return {
+    ...state,
+    name: action.name
+  }
+}
+
+export default createReducer(INITIAL_STATE, {
+  [Types.FILTER_TYPE]: setFilterType,
+  [Types.FILTER_NAME_ELEMENT]: setFilterNameElement,
+})
---
+++ src/pages/Index/ducks/index.ts
@@ -0,0 +1,13 @@
+import { all, put, takeLatest } from "redux-saga/effects";
+
+import { Creators as CreatorsFilter, FilterTypeAction, Types as TypesFilter } from './filters'
+
+function* sagaSetFilterType( action: FilterTypeAction ) {
+  yield put(CreatorsFilter.setFilterType(action.type))
+}
+
+export default function* rootSaga() {
+  return yield all([
+    yield takeLatest(TypesFilter.FILTER_TYPE, sagaSetFilterType)
+  ])
+}
